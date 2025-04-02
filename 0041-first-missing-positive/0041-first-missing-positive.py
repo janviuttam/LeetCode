17f939e53505c047a27734e2545c598057bbcf31
janviuttam <janviuttam2005@gmail.com>
2025-04-02 23:09:26 +0530

    Time: 45 ms (65.95%), Space: 20.5 MB (55.5%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()  # Sort the array
+        numToBeAt = 1
+        
+        for num in nums:
+            if num < 1:
+                continue
+            if num == numToBeAt:
+                numToBeAt += 1
+            elif num > numToBeAt:
+                return numToBeAt
+        
+        return max(nums[-1] + 1, 1)
