5235a957f06fc75e46fafd70b9fee1c2678a7592
janviuttam <janviuttam2005@gmail.com>
2026-03-19 21:59:12 +0530

    Time: 5 ms (50.45%), Space: 101.9 MB (85.67%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    int n, m;
-
-    int solve(vector<vector<int>>& grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] == 0)
-            return 1;
-
-        if (grid[i][j] == -1)
-            return 0;
-
-        grid[i][j] = -1;
-
-        int ans = 0;
-        ans += solve(grid, i + 1, j);
-        ans += solve(grid, i - 1, j);
-        ans += solve(grid, i, j + 1);
-        ans += solve(grid, i, j - 1);
-
-        return ans;
-    }
-
     int islandPerimeter(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
+        int n = grid.size();
+        int m = grid[0].size();
+        int perimeter = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    return solve(grid, i, j);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1) {
+                    perimeter += 4;
+                    if(i > 0 && grid[i-1][j] == 1)
+                        perimeter -= 2;
+                    if(j > 0 && grid[i][j-1] == 1)
+                        perimeter -= 2;
                 }
             }
         }
-        return 0;
+
+        return perimeter;
     }
 };
