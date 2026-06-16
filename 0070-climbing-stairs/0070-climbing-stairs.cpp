1b740b6c6e3a7d3ca218568def3b5c445e5e0d65
janviuttam <janviuttam2005@gmail.com>
2026-06-17 01:29:53 +0530

    Time: 0 ms (100%), Space: 7.9 MB (71.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        if (n <= 2) return n;
+    int t[46];
+    int solve(int n) {
+        if(n<0)
+            return 0;
+        if(t[n]!=-1)
+            return t[n];
+        if(n==0)
+            return 1;
+        
+        int one_step=solve(n-1);
+        int two_step=solve(n-2);
 
-        int prev2 = 1; 
-        int prev1 = 2; 
-
-        for (int i = 3; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
-        }
-
-        return prev1;
+        return t[n]=one_step + two_step;
+    }
+    int climbStairs(int n){
+        memset(t,-1,sizeof(t));
+        return solve(n);
     }
 };
